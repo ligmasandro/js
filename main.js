f4632f62d0f210348d7ad11fa4b4143e0cb58711
ligmasandro <sandrokashiashvili1@Gmail.com>
2025-03-01 03:16:39 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,106 @@
+const user1 = "mike";
+
+
+const user2 = "alpha";
+const age2 = 6996969696969696969;
+
+
+
+const num1 = 667;
+const num2 = -99;
+const num3 = 5+7;
+const num4 = 81-12;
+const num5 = 23 * 3;
+const num6 = 690 / 10
+
+
+console.log(user1);
+console.log(num1);
+console.log(num2);
+console.log(num3)
+console.log(num4);
+console.log(num5);
+console.log(num6);
+
+
+
+let age1 = 12;
+age1 = 13;
+console.log(age1);
+let color1 = "red"
+color1 = "royal cyan"
+console.log(color1);
+
+const s = "my name is optimus prome we are autobots";
+console.log(s);
+
+const h2 = document.querySelector(".head-2");
+console.log(h2);
+
+h2.style.color = "green";
+h2.style.backgroundColor = "cyan";
+h2.style.borderRadius = "20px"
+h2.style.height = "100px"
+h2.style.width = "100px"
+h2.style.padding = "40px"
+
+
+h2.remove();
+const button = document.querySelector(".btn");
+console.log(button);
+button.style.color = "red";
+button.style.backgroundColor = "green";
+button.style.borderRadius = "20px"
+button.style.height = "500px"
+button.style.width = "600px"
+button.style.padding = "50px"
+button.style.fontSize = "50px"
+
+const p = document.querySelector(".ku-iopta");
+console.log(p);
+p.style.color = "white";
+p.style.backgroundColor = "black";
+p.style.borderRadius = "20px"
+p.style.height = "500px"
+p.style.width = "600px"
+p.style.padding = "50px"
+p.style.fontSize = "50px"
+p.style.border = "19px solid cyan"
+p.style.justufyContant = "center"
+
+
+const input = document.querySelector(".in-put");
+console.log(input);
+input.style.color = "cyan";
+input.style.backgroundColor = "black";
+input.style.borderRadius = "20px"
+input.style.height = "500px"
+input.style.width = "800px"
+input.style.padding = "60px"
+input.style.fontSize = "50px"
+input.style.border = "19px solid purple"
+input.style.justufyContant = "center"
+
+
+
+const removeP = document.querySelector(".remove-p");
+console.log(removeP);
+
+const removeBtn = document.querySelector(".remove-btn");
+console.log(removeBtn);
+
+
+removeBtn.addEventListener("clicked" , () => {
+    console.log("btn was clicked");
+const num = 1
+removeBtn.remove()
+console.log(num);
+console.log("btn was clicked");
+
+)
+
+
+
+
+
+
